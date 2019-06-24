888da321496e3d2b381fd6a121d2b53222c88106
Fede <fede.arzuaga.perdomo@gmail.com>
2019-06-23 22:05:04 -0300

    Completando la clase de metodos para operar con strings

--- funciones_strings.php
+++ funciones_strings.php
@@ -24,6 +24,27 @@ echo "Metodo strpos(): Este metodo se utiliza para encontrar texto. Recibe dos p
 
 // Reemplazar texto
 
-echo "Metodo str_replace(search, replace, string) Recibe 3 parametros: lo que queremos reemplazar, lo que queremos poner en su lugar, y el string que va a ser manipulado => ".str_replace("aprender", "olvidar", $string)."</br></br>";
+echo "Metodo str_replace(search, replace, string) Recibe 3 parametros: lo que queremos reemplazar, lo que queremos poner en su lugar, y el string que va a ser manipulado => ".str_replace("aprender", "olvidar", $string);
+
+// Convertir a minusculas
+
+echo "Metodo strtolower(): Convierte toda la cadena de texto a minusculas => ".strtolower($string);
+
+// Convertir a mayusculas
+
+echo "Metodo strtoupper(): Convierte toda la cadena de texto a mayusculas => ".strtoupper($string);
+
+// Comparar cadenas
+
+echo "Metodo strcmp(): Compara las cadenas de texto preseleccionadas en cuanto a peso en bytes. Si la primera es mas grande que la ultima, dara un numero positivo y viceversa => ".strcmp("zorro", "j")."</br></br>";
+
+// Substraer cadenas
+
+echo "Metodo substr(): Extrae una porcion del texto que le hayamos indicado. Este metodo recibe tres parametros: el string a manipular, el indice del texto en donde tiene que empezar a extraer, y si le pasamos un tercer parametro tambien numerico, contara la cantidad de caracteres a extraer a partir del indice que nosotros le hayamos especificado => ".substr($string, 12, 19),"</br></br>";
+
+// Remover espacios en blanco
+
+echo "Metodo trim(): Remueve los espacios en blanco que PHP vea innecesarios (muy util para la entrada de datos) => ".trim("          
+	Esto          es           un            texto          con            muchos          espacios            ")."</br></br>";
 
 ?>
