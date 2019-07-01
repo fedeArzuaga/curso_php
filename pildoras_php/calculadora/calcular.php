abedb592518410d8b75bbfe775d69e90694ba664
Fede <fede.arzuaga.perdomo@gmail.com>
2019-07-01 19:31:50 -0300

    Agregando algunos archivos al curso de PHP y MySQL

---
+++ pildoras_php/calculadora/calcular.php
@@ -0,0 +1,29 @@
+<?php
+
+		if (isset($_POST['valor_uno'])) {
+			
+			$valor_uno = $_POST['valor_uno'];
+			$valor_dos = $_POST['valor_dos'];
+			$operacion = $_POST['operacion'];
+
+			calulcar($operacion, $valor_uno, $valor_dos);
+
+		}
+
+		function calulcar($calculo, $valor_uno, $valor_dos){
+
+			if( $calculo == "sumar" ){
+				echo "<p class=\"resultado\">El resultado de tu operacion ($calculo) es: ".($valor_uno + $valor_dos)."</p>";
+			}else if( $calculo == "restar" ){
+				echo "<p class=\"resultado\">El resultado de tu operacion ($calculo) es: ".($valor_uno - $valor_dos)."</p>";
+			}else if( $calculo == "dividir" ){
+				echo "<p class=\"resultado\">El resultado de tu operacion ($calculo) es: ".($valor_uno / $valor_dos)."</p>";
+			}else if( $calculo == "multiplicar" ){
+				echo "<p class=\"resultado\">El resultado de tu operacion ($calculo) es: ".($valor_uno * $valor_dos)."</p>";
+			}else if( $calculo == "modulo" ){
+				echo "<p class=\"resultado\">El resultado de tu operacion ($calculo) es: ".($valor_uno % $valor_dos)."</p>";
+			}
+
+		}
+
+	?>
---
+++ pildoras_php/funciones_matematicas.php
@@ -0,0 +1,41 @@
+<?php
+
+/* Funciones matematicas de PHP */
+
+// Esta funcion nos lanza un numero aleatorio (si le pasaramos 2 parametros numericos diferentes, la funcion lanza un numero aleatorio siempre comprendido entre esos dos valores que hemos pasado como parametro).
+$num1 = rand(10, 60);
+
+//Esta funcion sirve para hayar la potencia de un numero
+$num2 = pow(5,10);
+
+$num3 = 5.9821845641;
+
+echo "El numero es: " . $num1 . "<br>";
+echo "El numero exponenciado es: " . $num2 . "<br>";
+echo "El numero redondeado es: " . round($num3, 2) . "<br>";
+
+/* Casting en PHP */
+
+// A diferencia de otros lenguajes, PHP tiene un casting implicito, que significa eso: Que el propio lenguaje (segun las reglas ya vistas con los tipos de datos) determina que tipo de dato tiene una variable. Si le pasamos un numero sin comillas, PHP lo identifica como un integer. Si pasamos un texto entre comillas, PHP lo identifica como un string. Por ejemplo. Aun si, por ejemplo, tenemos una variable llamada $num4 que contiene el valor "5", de esta forma:
+
+$num4 = "5";
+
+// A pesar de que el contenido o el dato de la variable esta entre comillas, PHP identifica el numero, haciendo posible que el mismo se comporte como tal. De esa forma nosotros podemos realizar operaciones con esa variable como por ejemplo:
+
+$num4 += 2; // Le sumo dos
+
+echo $num4 . "<br>";
+
+// Output => 7 (checalo)
+
+#####################################################################
+
+/* Casting Explicito */
+
+$num5 = "15";
+
+$resultado = (int) $num5;
+
+echo $resultado;
+
+?>
